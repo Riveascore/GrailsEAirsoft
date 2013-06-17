a2b7e9b3962e954d20c82124967890f5e9cbc648
riveascore <agaxx010@morris.umn.edu>
2013-06-17 16:51:00 -0500

    Got passphrase requirements to work with cosntraints using validator. Next, need to make sure:
    setup()
    cleanup()
    mockDomain()
    etc. 
    
    Usage is correct.

--- CombatCabana/grails-app/domain/core/User.groovy
+++ CombatCabana/grails-app/domain/core/User.groovy
@@ -31,10 +31,18 @@ class User {
 	//UP UNTIL you want to sell/buy, then you have to enter
 	//The process of adding a credit card, shipping point etc would insert 
 	//Address etc for you
-		
+	
+	boolean equals(user){
+		if(user.getClass() != User){
+			return false
+		}
+		if(user.id != this.id){
+			return false
+		}
+		true
+	}
+	
     static constraints = {
-//		strongEnoughPassword()
-		
 		//Find a way to make this nicer
 		cart nullable:true
 		store nullable:true
@@ -46,25 +54,15 @@ class User {
 		address nullable:true
 		phoneNumber nullable:true
 		
-		// Might be covered by authenticationUserClass?
-		//login blank: false, size: 1..20, unique: true
-		//email nullable: false, email: true
-		//dateJoined nullable: true
-		// min:3
+		//Make sure password is at least 3 words to ensure hard to crack password :D
+		password blank:false, validator: { value, obj ->
+			int numberOfWords = value.collect {
+				it.charAt(0).digit || it.charAt(0).letter ? it : ' '
+				}.join('').tokenize(' ').size()
+				
+			if(numberOfWords < 3){
+				return 'className.propertyName.passphrase.validation.msg'
+			}
+		}
     }
-	
-	boolean strongEnoughPassword(){
-		int numberOfWords = password.collect { it.charAt(0).digit || it.charAt(0).letter ? it : ' ' }.join('').tokenize(' ').size()
-		numberOfWords >= 3
-	}
-	
-	boolean equals(user){
-		if(user.getClass() != User){
-			return false
-		}
-		if(user.id != this.id){
-			return false
-		}
-		true
-	}
 }
